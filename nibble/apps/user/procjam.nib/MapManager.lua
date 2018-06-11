f63b469ff2e65139b26a97826b0cc87ac1549882
Pâmela Beltrani <beltrani_assis@hotmail.com>
2018-06-10 22:21:53 -0300

    MapManager Isolado
    O MapManager foi Isolado para forma humilde que merece...

--- nibble/apps/user/procjam.nib/MapManager.lua
+++ nibble/apps/user/procjam.nib/MapManager.lua
@@ -1,33 +1,98 @@
-local MapManager = {}
+-- Functions to generate and control the dungeons.
+-- For Room generation, see the RoomManager
 
-function MapManager:new(text, x, y)
-    local instance = {
-        text = text or "",
-        x = x or 0, y = y or 0,
-        palette = 0,
-        color = 15,
-        background_color = 0,
-        bold = 0,
-        underline = false,
-        align = 0
-    }
+	-- In Spelunky the level is made of an 4x4 grid. There are 4 different basic rooms:
+	-- 0 : A side room that is not on solution path
+	-- 1 : A room that is guaranteed to have a left exit and a right exit
+	-- 2 : A room that is guaranteed to have exists on the left, right and bottom. 
+	--		If there's another "2" room above it, then it also is guaranteed a top exit
+	-- 3 : A room that is guaranteed to have exists on the, left, right and top
 
-    lang.instanceof(instance, self)
-
-    return instance
+function generateLevel(size_x, size_y)	
+	local tilemap = {}
+	
+	-- Just to be safe,
+	-- first we create all the rooms as an empty rooms without exits
+	for row = 0, size_y do
+		for column = 0, size_x do
+			tilemap[row * size_x + column] = 0
+		end
+	end
+	
+	-- Here we start the process to generate all the level
+	row = 0
+	column = 0
+	
+	-- First, pick a random room on the first row to be the entrace
+	column = math.random(0,size_x)
+	dprint('Entrace:' .. column)
+	tilemap[column] = math.random(1,2)
+	
+	-- After put the first room, we decide if were should we go:
+	-- 1 or 2 	: right
+	-- 3 or 4 	: left
+	-- 5 		: down	
+	direction = math.random(1,5)
+	
+	-- When a direction is decided, then we can start to create the rooms
+	while (true) do
+		dprint('Actual Row:' .. row .. '\t Column:' .. column .. '\t Direction:' .. direction .. '\t Tile:' .. tilemap[row * size_x + column])
+		if direction == 1 or direction == 2 then		-- right
+			if column - 1 > 0 then
+				column -= 1
+				tilemap[row * size_x + column] = math.random(1,3)
+			else				-- If is on the extreme right, go down and change direction
+				tilemap[row * size_x + column] = 2
+				row += 1
+				direction = 3
+			end
+		elseif direction == 3 or direction == 4 then	-- left
+			if column + 1 <= size_x then
+				column += 1
+				tilemap[row * size_x + column] = math.random(1,3)
+			else				-- If is on the extreme left, go down and change direction
+				tilemap[row * size_x + column] = 2 
+				row += 1
+				direction = 1
+			end
+		else											-- down
+			if row < size_y then
+				tilemap[row * size_x + column] = 2
+				tilemap[row * size_x + column] = 2
+				row += 1
+			end
+		end
+		
+		if row > size_y then
+			break
+		end
+		
+		dprint('Next Row:' .. row .. '\t Column:' .. column .. '\t Direction:' .. direction .. '\t Tile:' .. tilemap[row * size_x + column])
+	end
+	return tilemap
 end
 
+function isInsideMap(x, y, size_x, size_y)
+	return x > 0 and x <= size_x and y > 0 and y <= size_y
+end
 
-return MapManager
+function printTilemap(t, size_x, size_y)
+	local s = ''
+	for row = 0, size_y do
+		for column = 0, size_x do
+			s = s .. tilemap[row * size_x + column]
+		end
+		dprint (s)
+		s = ''
+	end
+end
 
+function chooseNewDirection(actualDirection)
+	local newDirection = math.random(1,5)
+	while( newDirection ~= actualDirection and newDirection ~= 5) do
+		newDirection = math.random(1,5)
+		dprint(newDirection)
+	end
 
---MapManager
---RoomManager
---	tileManager
-
---ParticleManager
-
---Entity
---Enemy
---Player
---Item
+	return newDirection
+end
--- nibble/apps/user/procjam.nib/main.lua
+++ nibble/apps/user/procjam.nib/main.lua
@@ -1,84 +1,5 @@
 local mapManager = require('MapManager')
 
-function generateLevel(size_x, size_y)	
-	-- In Spelunky the level is made of an 4x4 grid. There are 4 different basic rooms:
-	-- 0 : A side room that is not on solution path
-	-- 1 : A room that is guaranteed to have a left exit and a right exit
-	-- 2 : A room that is guaranteed to have exists on the left, right and bottom. 
-	--		If there's another "2" room above it, then it also is guaranteed a top exit
-	-- 3 : A room that is guaranteed to have exists on the, left, right and top
-	
-	local tilemap = {}
-	
-	-- Just to be safe,
-	-- first we create all the rooms as an empty rooms without exits
-	for row = 0, size_y do
-		for column = 0, size_x do
-			tilemap[row * size_x + column] = 0
-		end
-	end
-	
-	-- Here we start the process to generate all the level
-	row = 0
-	column = 0
-	
-	-- First, pick a random room on the first row to be the entrace
-	column = math.random(0,size_x)
-	dprint('Entrace:' .. column)
-	tilemap[column] = math.random(1,2)
-	
-	-- After put the first room, we decide if were should we go:
-	-- 1 or 2 	: right
-	-- 3 or 4 	: left
-	-- 5 		: down	
-	-- When a direction is decided, then we can start to create the rooms
-	direction = math.random(1,5)
-	while (true) do
-		if direction == 1 or direction == 2 then		-- right
-			if column - 1 > 0 then
-				column -= 1
-			else				-- If is on the extreme right, go down and change direction
-				tilemap[row * size_x + column] = 2
-				row += 1
-				direction = 3
-			end
-		elseif direction == 3 or direction == 4 then	-- left
-			if column + 1 < size_x then
-				column += 1
-			else				-- If is on the extreme left, go down and change direction
-				tilemap[row * size_x + column] = 2 
-				row += 1
-				direction = 1
-			end
-		else											-- down
-			if row + 1 < size_y then
-				row += 1
-			else			-- Then just create the end room
-				break
-			end
-		end
-		tilemap[row * size_x + column] = 1 -- Jus putting 1 while dont run smoothly
-	end
-	
-	return tilemap
-end
-
-function isInsideMap(x, y, size_x, size_y)
-	return x > 0 and x < size_x and y > 0 and y < size_y
-end
-
-
-function printTilemap(t, size_x, size_y)
-	local s = ''
-	for row = 0, size_y do
-		for column = 0, size_x do
-			s = s .. tilemap[row * size_x + column]
-		end
-		dprint (s)
-		s = ''
-	end
-end
-
 local tile_w, tile_h = 16, 16
 local tilemap_w, tilemap_h = 20, 15 
 
@@ -127,7 +48,7 @@ function init()
     mask(0)
 	
 	-- Getting a seed from the OS
-	math.randomseed( 100 )
+	math.randomseed( 115 )
 	
 	tilemap = generateLevel(20,15)
 end
@@ -151,7 +72,7 @@ function draw()
     end
 
     -- Draws player
-    spr(player_x, player_y, 6, 1)
+    --spr(player_x, player_y, 6, 1)
 
     -- Draws collision check
     rect(check_x1*tile_w, check_y1*tile_h, tile_w, tile_h, 15)
